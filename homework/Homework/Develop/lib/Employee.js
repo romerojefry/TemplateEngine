b96dd3756a83876c04f8334861e6fdd63420e826
Jefry Romero <romerojefry@icloud.com>
2020-10-13 19:03:17 -0700

    adding constructor for employee.js

--- homework/Homework/Develop/lib/Employee.js
+++ homework/Homework/Develop/lib/Employee.js
@@ -1,1 +1,9 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    construstor (name, id, email){
+        this.name = name,
+        this.id = id, 
+        this.email = email,
+    }
+}
